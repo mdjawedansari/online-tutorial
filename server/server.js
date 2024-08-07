ddb34ad13525df56702e85aee71cf29706e3addc
mdjawedansari <116138541+mdjawedansari@users.noreply.github.com>
2024-08-07 16:46:55 +0530

    a

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 import jsonServer from 'json-server'
+import cors from 'cors'
+import path from 'path'
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); // Adjust the path to your db.json
+const router = jsonServer.router(path.join(__dirname, 'db.json')); // Adjust the path to your db.json
 const middlewares = jsonServer.defaults();
 const port = 5000;
+
+server.use(cors()); // Enable CORS
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
 
 server.use(middlewares);
 server.use(router);
